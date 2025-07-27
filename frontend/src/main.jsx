d621d3921dd7570d36b2ddaf5ad5e02b39fe1b46
Alsycode <120390865+Alsycode@users.noreply.github.com>
2025-07-27 23:05:33 +0530

    27-07

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -20,6 +20,14 @@ const router = createBrowserRouter([
     path: "/",
     element: <div>Hello world!</div>,
   },
+      {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+      {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
 ]);
 createRoot(document.getElementById('root')).render(
   <StrictMode>
